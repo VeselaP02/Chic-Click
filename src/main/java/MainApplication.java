6c1c1de79568f985bc204db284d5b49f68616196
VeselaP02 <vpetkova0204@abv.bg>
2025-02-01 00:21:16 +0200

    update project using spring boot

---
+++ src/main/java/MainApplication.java
@@ -0,0 +1,12 @@
+//import entities.Book;
+//import entities.User;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class,args);
+    }
+
+}
